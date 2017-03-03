ae2f4380260a87177058c00ca1d14f981a9a4799
Alicia Rodriguez Taboada <alicia_rodriguez@ultimatesoftware.com>
2017-03-03 00:41:33 -0500

    implement rnfr, rnto, type, and dele. refactor msg to cmd.

--- ftp_server.py
+++ ftp_server.py
@@ -12,7 +12,10 @@ import os
 thread_list = []
 RECV_BUFFER = 1024
 current_directory = os.path.abspath(".")
-base_directory = os.path.abspath(".")
+base_directory = os.path.abspath("/")
+rename_from_path = ""
+rename_from_file = ""
+type = "A"
 
 # Commands
 
@@ -41,62 +44,65 @@ def server_thread(connection_socket, address):
 
     global current_directory
     global base_directory
+    global type
 
     try:
         print("Thread Server Entering Now...")
         print(address)
         local_thread = threading.local()
+        connection_socket.send(str_msg_encode("220 FTP Server v1.0\r\n"))
 
         while True:
 
             print("Current Directory: " + current_directory)
             print("Base Directory: " + base_directory)
+            print("Type: " + type)
             print("TID = ", threading.current_thread())
 
-            msg = str_msg_decode(connection_socket.recv(RECV_BUFFER))
+            cmd = str_msg_decode(connection_socket.recv(RECV_BUFFER))
 
-            print("Received command: " + msg)
+            print("Received command: " + cmd)
 
-            if msg[0:4] == CMD_QUIT:
+            if cmd[0:4] == CMD_QUIT:
                 quit_ftp(connection_socket, local_thread)
-            elif msg[0:4] == CMD_USER:
-                user_ftp(connection_socket, local_thread, msg)
-            elif msg[0:4] == CMD_PASS:
-                pass_ftp(connection_socket, local_thread, msg)
-            elif msg[0:3] == CMD_CWD:
-                cwd_ftp(connection_socket, local_thread, msg)
-            elif msg[0:4] == CMD_CDUP:
+            elif cmd[0:4] == CMD_USER:
+                user_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:4] == CMD_PASS:
+                pass_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:3] == CMD_CWD:
+                cwd_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:4] == CMD_CDUP:
                 cdup_ftp(connection_socket, local_thread)
-            elif msg[0:4] == CMD_RETR:
-                retr_ftp(connection_socket, local_thread, msg)
-            elif msg[0:4] == CMD_STOR:
-                stor_ftp(connection_socket, local_thread, msg)
-            elif msg[0:4] == CMD_STOU:
-                stou_ftp(connection_socket, local_thread, msg)
-            elif msg[0:4] == CMD_APPE:
-                appe_ftp(connection_socket, local_thread, msg)
-            elif msg[0:4] == CMD_TYPE:
-                type_ftp(connection_socket, local_thread, msg)
-            elif msg[0:4] == CMD_RNFR:
-                rnfr_ftp(connection_socket, local_thread, msg)
-            elif msg[0:4] == CMD_RNTO:
-                rnto_ftp(connection_socket, local_thread, msg)
-            elif msg[0:4] == CMD_DELE:
-                dele_ftp(connection_socket, local_thread, msg)
-            elif msg[0:3] == CMD_RMD:
-                rmd_ftp(connection_socket, local_thread, msg)
-            elif msg[0:3] == CMD_MKD:
-                mkd_ftp(connection_socket, local_thread, msg)
-            elif msg[0:3] == CMD_PWD:
+            elif cmd[0:4] == CMD_RETR:
+                retr_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:4] == CMD_STOR:
+                stor_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:4] == CMD_STOU:
+                stou_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:4] == CMD_APPE:
+                appe_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:4] == CMD_TYPE:
+                type_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:4] == CMD_RNFR:
+                rnfr_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:4] == CMD_RNTO:
+                rnto_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:4] == CMD_DELE:
+                dele_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:3] == CMD_RMD:
+                rmd_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:3] == CMD_MKD:
+                mkd_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:3] == CMD_PWD:
                 pwd_ftp(connection_socket, local_thread)
-            elif msg[0:4] == CMD_LIST:
-                list_ftp(connection_socket, local_thread, msg)
-            elif msg[0:4] == CMD_NOOP:
+            elif cmd[0:4] == CMD_LIST:
+                list_ftp(connection_socket, local_thread, cmd)
+            elif cmd[0:4] == CMD_NOOP:
                 noop_ftp(connection_socket, local_thread)
-            elif msg[0:4] == CMD_PORT:
-                port_ftp(connection_socket, local_thread, msg)
+            elif cmd[0:4] == CMD_PORT:
+                port_ftp(connection_socket, local_thread, cmd)
             else:
-                local_thread = ("You said what? " + msg)
+                local_thread = ("You said what? " + cmd)
                 print(local_thread)
                 connection_socket.send(str_msg_encode(local_thread))
     except OSError as e:
@@ -110,23 +116,23 @@ def quit_ftp(connection_socket, local_thread):
     connection_socket.close()
 
 
-def user_ftp(connection_socket, local_thread, msg):
-    local_thread = "331 Password required for " + msg[4:-1] + "\r\n"
+def user_ftp(connection_socket, local_thread, cmd):
+    local_thread = "331 Password required for " + cmd[4:-1] + "\r\n"
     connection_socket.send(str_msg_encode(local_thread))
 
 
-def pass_ftp(connection_socket, local_thread, msg):
+def pass_ftp(connection_socket, local_thread, cmd):
     # TODO: if user is part of the user
     local_thread = "230 Anonymous access granted, restrictions apply\r\n"
     connection_socket.send(str_msg_encode(local_thread))
 
 
 # Change Working Directory (CWD)
-def cwd_ftp(connection_socket, local_thread, msg):
+def cwd_ftp(connection_socket, local_thread, cmd):
     global current_directory
     global base_directory
 
-    directory = msg[4:-1]
+    directory = cmd[4:-1]
 
     if directory == "/":
         current_directory = base_directory
@@ -163,43 +169,80 @@ def cdup_ftp(connection_socket, local_thread):
     connection_socket.send(str_msg_encode(local_thread))
 
 
-def retr_ftp(connection_socket, local_thread, msg):
+def retr_ftp(connection_socket, local_thread, cmd):
     connection_socket.send(str_msg_encode(local_thread))
 
 
-def stor_ftp(connection_socket, local_thread, msg):
+def stor_ftp(connection_socket, local_thread, cmd):
     connection_socket.send(str_msg_encode(local_thread))
 
 
-def stou_ftp(connection_socket, local_thread, msg):
+def stou_ftp(connection_socket, local_thread, cmd):
     connection_socket.send(str_msg_encode(local_thread))
 
 
-def appe_ftp(connection_socket, local_thread, msg):
+def appe_ftp(connection_socket, local_thread, cmd):
     connection_socket.send(str_msg_encode(local_thread))
 
 
-def type_ftp(connection_socket, local_thread, msg):
+def type_ftp(connection_socket, local_thread, cmd):
+    global type
+
+    type = cmd[5]
+    local_thread = "200 Type set to " + type + "\r\n"
     connection_socket.send(str_msg_encode(local_thread))
 
 
-def rnfr_ftp(connection_socket, local_thread, msg):
+# ReName FRom (RNFR)
+def rnfr_ftp(connection_socket, local_thread, cmd):
+    global current_directory
+    global rename_from_path
+    global rename_from_file
+
+    rename_from_file = cmd[5:-1]
+    rename_from_path = os.path.join(current_directory, cmd[5:-1])
+    local_thread = "350 File or directory exists, ready for destination name\r\n"
     connection_socket.send(str_msg_encode(local_thread))
 
 
-def rnto_ftp(connection_socket, local_thread, msg):
+# ReName TO (RNTO)
+def rnto_ftp(connection_socket, local_thread, cmd):
+    global current_directory
+    global rename_from_path
+    global rename_from_file
+
+    if os.path.exists(rename_from_path):
+        rename_to = os.path.join(current_directory, cmd[5:-1])
+        os.rename(rename_from_path, rename_to)
+        local_thread = "250 Rename successful\r\n"
+    else:
+        local_thread = "550 " + rename_from_file + ": No such file or directory\r\n"
+
     connection_socket.send(str_msg_encode(local_thread))
 
 
-def dele_ftp(connection_socket, local_thread, msg):
+def dele_ftp(connection_socket, local_thread, cmd):
+    global current_directory
+
+    file = cmd[5:-1]
+    path = os.path.join(current_directory, file)
+
+    if os.path.isdir(path):
+        local_thread = "550 " + file + ": Is a directory\r\n"
+    elif os.path.exists(path):
+        os.remove(path)
+        local_thread = "250 DELE command successful\r\n"
+    else:
+        local_thread = "550 " + file + ": No such file or directory\r\n"
+
     connection_socket.send(str_msg_encode(local_thread))
 
 
 # ReMove Directory (RMD)
-def rmd_ftp(connection_socket, local_thread, msg):
+def rmd_ftp(connection_socket, local_thread, cmd):
     global current_directory
 
-    remove_directory = msg[4:-1]
+    remove_directory = cmd[4:-1]
     path = os.path.join(current_directory, remove_directory)
 
     if remove_directory == "":
@@ -214,10 +257,10 @@ def rmd_ftp(connection_socket, local_thread, msg):
 
 
 # MaKe Directory (MKD)
-def mkd_ftp(connection_socket, local_thread, msg):
+def mkd_ftp(connection_socket, local_thread, cmd):
     global current_directory
 
-    new_directory = msg[4:-1]
+    new_directory = cmd[4:-1]
     path = os.path.join(current_directory, new_directory);
 
     if not os.path.isdir(path):
@@ -247,13 +290,19 @@ def pwd_ftp(connection_socket, local_thread):
     connection_socket.send(str_msg_encode(local_thread))
 
 
-def list_ftp(connection_socket, local_thread, msg):
-    local_thread = "150 Opening ASCII mode data connection for file list"
-    connection_socket.send(str_msg_encode(local_thread))
+def list_ftp(connection_socket, local_thread, cmd):
+    global current_directory
 
-    # TODO: print list
+    # TODO: use subprocess.check_output()
 
-    local_thread = "226 Transfer complete"
+    local_thread = "150 Opening ASCII mode data connection for file list\r\n"
+    # connection_socket.send(str_msg_encode(local_thread))
+
+    for items in os.listdir(current_directory):
+        local_thread = local_thread + items + "\r\n"
+        # connection_socket.send(str_msg_encode(local_thread))
+
+    local_thread += "226 Transfer complete\r\n"
     connection_socket.send(str_msg_encode(local_thread))
 
 
@@ -262,7 +311,8 @@ def noop_ftp(connection_socket, local_thread):
     connection_socket.send(str_msg_encode(local_thread))
 
 
-def port_ftp(connection_socket, local_thread, msg):
+def port_ftp(connection_socket, local_thread, cmd):
+    local_thread = "200 PORT command successful\r\n"
     connection_socket.send(str_msg_encode(local_thread))
 
 
