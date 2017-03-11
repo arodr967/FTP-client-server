adb4a9e5827e90d6b69332d341caa50ab9d4f210
Alicia Rodriguez Taboada <alicia_rodriguez@ultimatesoftware.com>
2017-03-10 23:52:13 -0500

    get default type to binary, check if user is not logged in when creating data channel, update get command, create get_file_mode helper function, update ls command

--- ftp_client.py
+++ ftp_client.py
@@ -42,6 +42,7 @@ CMD_DISCONNECT = "DISCONNECT"
 CMD_CLOSE = "CLOSE"
 CMD_NOOP = "NOOP"
 CMD_LCD = "LCD"
+CMD_USAGE = "USAGE"
 
 # TODO
 
@@ -54,7 +55,7 @@ CMD_VERBOSE = "VERBOSE"
 CMD_DEBUG = "DEBUG"
 CMD_LLS = "LLS"
 CMD_LPWD = "LPWD"
-CMD_USAGE = "USAGE"
+
 
 # Parse arguments
 
@@ -121,9 +122,9 @@ else:
 # DATA_PORT_MIN = 60020
 # FTP_PORT = 21
 
-FTP_PORT = args.fp
-DATA_PORT_MAX = args.dpr.split("-")[1]
-DATA_PORT_MIN = args.dpr.split("-")[0]
+FTP_PORT = int(args.fp)
+DATA_PORT_MAX = int(args.dpr.split("-")[1])
+DATA_PORT_MIN = int(args.dpr.split("-")[0])
 DATA_PORT_BACKLOG = 1
 
 next_data_port = 1
@@ -133,7 +134,7 @@ password = args.w
 sunique = False
 current_directory = os.path.abspath(".")
 base_directory = os.path.abspath("/")
-set_type = "A"
+set_type = "I"
 config_file = args.config
 log_file = args.log
 log_all = args.ALL
@@ -307,10 +308,6 @@ def run_commands(tokens, logged_on, ftp_socket):
             return "[PUT] Failed to get data port. Try again.", logged_on, ftp_socket
 
     if cmd == CMD_GET or cmd == CMD_RECV:
-        # FTP must create a channel to received data before
-        # executing get.
-        # also makes sure that data_socket is valid
-        # in other words, not None
         data_socket = ftp_new_dataport(ftp_socket)
         if data_socket is not None:
             get_ftp(tokens, ftp_socket, data_socket)
@@ -343,7 +340,7 @@ def ftp_connecthost(hostname):
 
     ftp_socket = socket(AF_INET, SOCK_STREAM)
     ftp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    ftp_socket.connect((hostname, int(FTP_PORT)))
+    ftp_socket.connect((hostname, FTP_PORT))
 
     print("Connected to " + hostname)
 
@@ -356,41 +353,37 @@ def ftp_new_dataport(ftp_socket):
     data_port = next_data_port
     host = gethostname()
     host_address = gethostbyname(host)
-    next_data_port = next_data_port + 1
+    next_data_port += 1
     data_port = (DATA_PORT_MIN + data_port) % DATA_PORT_MAX
 
     data_socket = socket(AF_INET, SOCK_STREAM)
-    # reuse port
     data_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-
     data_socket.bind((host_address, data_port))
     data_socket.listen(DATA_PORT_BACKLOG)
 
-    # the port requires the following
-    # PORT IP PORT
-    # however, it must be transmitted like this.
-    # PORT 192,168,1,2,17,24
-    # where the first four octet are the ip and the last two form a port number.
     host_address_split = host_address.split('.')
-    high_data_port = str(data_port // 256)  # get high part
-    low_data_port = str(data_port % 256)  # similar to data_port << 8 (left shift)
+    high_data_port = str(data_port // 256)
+    low_data_port = str(data_port % 256)
     port_argument_list = host_address_split + [high_data_port, low_data_port]
     port_arguments = ','.join(port_argument_list)
     cmd_port_send = CMD_PORT + ' ' + port_arguments + "\r\n"
 
     try:
         ftp_socket.send(str_msg_encode(cmd_port_send))
-    except socket.timeout:
-        print("Socket timeout. Port may have been used recently. wait and try again!")
-        return None
-    except socket.error:
+    # except socket.timeout:
+    #     print("Socket timeout. Port may have been used recently. wait and try again!")
+    #     return None
+    except OSError as e:
         print("Socket error. Try again")
         return None
 
     msg = ftp_socket.recv(RECV_BUFFER)
     sys.stdout.write(str_msg_decode(msg, True))
 
-    return data_socket
+    if str_msg_decode(msg).split()[0] == "530":
+        return None
+    else:
+        return data_socket
 
 
 def noop_ftp(ftp_socket):
@@ -581,14 +574,18 @@ def rename_ftp(tokens, ftp_socket):
 
     ftp_socket.send(str_msg_encode("RNFR " + from_name + "\n"))
     msg = ftp_socket.recv(RECV_BUFFER)
-    sys.stdout.write(str_msg_decode(msg, True))
+
+    tokens = str_msg_decode(msg).split()
+
+    if tokens[0] != "530":
+        sys.stdout.write(str_msg_decode(msg, True))
 
     ftp_socket.send(str_msg_encode("RNTO " + to_name + "\n"))
     msg = ftp_socket.recv(RECV_BUFFER)
 
     tokens = str_msg_decode(msg).split()
 
-    if tokens[0] == "503":
+    if tokens[0] == "503" or tokens[0] == "550":
         return
     else:
         sys.stdout.write(str_msg_decode(msg, True))
@@ -598,45 +595,50 @@ def get_ftp(tokens, ftp_socket, data_socket):
 
     if len(tokens) is 1:
         remote_file = input("(remote-file) ")
+        local_file = input("(local-file) ")
     elif len(tokens) is 2:
         remote_file = tokens[1]
-        local_file = input("(local-file) ")
-    elif len(tokens) is 3:
-        remote_file = tokens[1]
-        local_file = tokens[2]
+        local_file = remote_file
     else:
         remote_file = tokens[1]
         local_file = tokens[2]
 
+    print("local: " + local_file + " remote: " + remote_file)
+
     ftp_socket.send(str_msg_encode("RETR " + remote_file + "\n"))
 
     msg = ftp_socket.recv(RECV_BUFFER)
-    str_value = str_msg_decode(msg)
-    tokens = str_value.split()
+    tokens = str_msg_decode(msg).split()
+
     if tokens[0] != "150":
-        print("Unable to retrieve file. Check that file exists (ls) or that you have permissions")
+        sys.stdout.write(str_msg_decode(msg, True))
         return
 
-    print(str_msg_decode(msg, True))
-
     data_connection, data_host = data_socket.accept()
-    file_bin = open(local_file, "wb")  # read and binary modes
 
+    file = open(local_file, get_file_mode("w"))
     size_recv = 0
+
     while True:
         data = data_connection.recv(RECV_BUFFER)
-
-        if not data or data == '' or len(data) <= 0:
-            file_bin.close()
-            break
-        else:
-            file_bin.write(data)
+        if len(data) < RECV_BUFFER:
+            file.write(data)
             size_recv += len(data)
+            break
+        file.write(data)
 
     data_connection.close()
 
     msg = ftp_socket.recv(RECV_BUFFER)
-    print(str_msg_decode(msg, True))
+    sys.stdout.write(str_msg_decode(msg, True))
+
+
+def get_file_mode(mode):
+    global set_type
+    if set_type == "A":
+        return mode
+    else:
+        return mode + "b"
 
 
 def put_ftp(tokens, ftp_socket, data_socket):
@@ -698,15 +700,19 @@ def ls_ftp(tokens, ftp_socket, data_socket):
 
     data_connection, data_host = data_socket.accept()
 
-    msg = data_connection.recv(RECV_BUFFER)
-    while len(msg) > 0:
-        sys.stdout.write(str_msg_decode(msg, True))
+    while True:
         msg = data_connection.recv(RECV_BUFFER)
+        if len(msg) < RECV_BUFFER:
+            sys.stdout.write(str_msg_decode(msg, True))
+            break
+        sys.stdout.write(str_msg_decode(msg, True))
 
     data_connection.close()
-
-    msg = ftp_socket.recv(RECV_BUFFER)
-    sys.stdout.write(str_msg_decode(msg, True))
+    if str_msg_decode(msg).split()[0] == "450":
+        return
+    else:
+        msg = ftp_socket.recv(RECV_BUFFER)
+        sys.stdout.write(str_msg_decode(msg, True))
 
 
 def delete_ftp(tokens, ftp_socket):
